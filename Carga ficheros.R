dbdcc7233b41d444b28088a4da6a6b7f76bf1330
Paula <paula.hermosilla@alumni.mondragon.edu>
2020-05-22 10:00:04 +0200

    Eliminamos las filas que no queremos

--- Carga ficheros.R
+++ Carga ficheros.R
@@ -2,3 +2,8 @@ setwd("C:/Users/Paula/Desktop/Examen_DS/Archivos_examen_ds")
 datos<-load("muestra20.RData", envir = parent.frame(), verbose = FALSE)
 names(muestra20)<- c("nif", "nombre", "genero")
 names(muestra20)
+
+#NIF
+regexp <- "([[:digit:]]{8}) ([[:alpha:]]{1})"
+grepl(pattern = regexp, x = muestra20$nif)
+muestra20<-muestra20[-c(1:26),]
