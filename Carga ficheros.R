9e0de05bb741e185c1d986f06e37ae491ec4d21d
Paula <paula.hermosilla@alumni.mondragon.edu>
2020-05-22 09:45:51 +0200

    Cambiamos nombres de las columnas

---
+++ Carga ficheros.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/Paula/Desktop/Examen_DS/Archivos_examen_ds")
+datos<-load("muestra20.RData", envir = parent.frame(), verbose = FALSE)
+names(muestra20)<- c("nif", "nombre", "genero")
+names(muestra20)
